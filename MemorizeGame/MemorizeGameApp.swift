5bc9aff72f9357250a43fc0a21bf5cb1be042f4c
tslsun <smartsunnytsai@gmail.com>
2021-06-13 07:31:43 +0800

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by 蔡松霖 on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
